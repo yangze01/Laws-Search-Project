d1fff57a9298aa0d883a1b988aa98804d0ab00c1
yangze01 <858848101@qq.com>
2017-11-16 11:59:58 +0800

    add save

--- correlation.py
+++ correlation.py
@@ -32,21 +32,69 @@ def make_dictionary(seg_corpus, dictionary_file):
     print("make dictionary finished")
     return dictionary
 
+def doc2bag_corpus(dictionary, seg_corpus):
+
+    bag_of_words_corpus = [dictionary.doc2bow(pdoc) for pdoc in seg_corpus]
+    print("bag_of words finished finished")
+    return bag_of_words_corpus
+
+def save_json(save_path, data):
+    with open(save_path, 'w+') as f:
+        json.dump(data, f, ensure_ascii = False)
+    print("json data saved as {}".format(save_path))
+
+
 
 if __name__ == "__main__":
     print(1)
-    # 将数据保存为词袋模型
+
+    # 将数据库保存为词袋模型
     with open(BasePath + "/seg_corpus/data_corpus0.json", 'rb') as fp:
         data = json.load(fp)
     print(data.keys())
-
-    # 将数据库保存为词袋模型
+    corpus = data['content_wordlist']
     dictionary_path = BasePath + "/correlation_data/dict.pickle"
-    dictionary = make_dictionary(data['content_wordlist'], dictionary_path)
+    dictionary = make_dictionary(corpus, dictionary_path)
     print("the dictionary len is : {}".format(len(dictionary)))
-    id2token = {value: key for key, value in dictionary.token2id.items()}
+    # id2token = {value: key for key, value in dictionary.token2id.items()}
     # print(id2token)
-    print(data['content_wordlist'])
+    print(' '.join(data['content_wordlist'][0]))
+    print("the len of dictionary: {}".format(len(dictionary.token2id)))
+    bag_corpus = doc2bag_corpus(dictionary, corpus)
+    bag_sen_dict = dict()
+    for i in range(0, len(bag_corpus)):
+        print(i)
+        tmp_sen = bag_corpus[i]
+        bag_sen = [word_tuple[0] for word_tuple in tmp_sen]
+        # print(tmp_sen)
+        # print(bag_sen)
+        bag_sen_dict[i+1] = bag_sen
+
+    print("the len of bag_sen_dict is : {}".format(len(bag_sen_dict)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
